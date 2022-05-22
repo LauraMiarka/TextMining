6af4cc32eeed8ff96df024f07f7a06921c955a7f
LauraMiarka <93878018+LauraMiarka@users.noreply.github.com>
2022-05-22 10:21:47 +0200

    Delete tokenizer.py

--- tokenizer.py
+++
@@ -1,7 +0,0 @@
-from cleaning import text_cleaner
-from stemming import stem_text
-
-def text_tokenizer(text):
-    text = text_cleaner(text)
-    text = stem_text(text)
-    return [word for word in text if len(word)>3]
